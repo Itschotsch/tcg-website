858b575967056762fe911d1999a176a7c9bf9da6
Jan Appel <jan.appel@nzz.ch>
2026-08-07 15:33:46 +0200

    FAQ update

--- app/page-home.ts
+++ app/page-home.ts
@@ -115,50 +115,46 @@ export namespace Website {
                 "faqItems": [
                     {
                         "question": "Was ist Anor?",
-                        "answer": "Anor ist ein LARP-Sammelkartenspiel, das Geschichten, Charaktere und Ereignisse aus unterschiedlichen LARP-Kampagnen und Settings in Kartenform festhält. Jede Karte repräsentiert Charaktere, Lore oder Ereignisse aus verschiedenen LARP-Settings."
+                        "answer": "Anor ist ein <a href=\"https://de.wikipedia.org/wiki/Sammelkartenspiel\" target=\"_blank\">Sammelkartenspiel</a>. Es spielt sich ähnlich und doch erstaunlich anders als andere bekannte Sammelkartenspiele. Dazu kommt: Inhaltlich erzählt es von Geschichten, Charakteren und Momenten aus den unterschiedlichsten <a href=\"https://de.wikipedia.org/wiki/Live_Action_Role_Playing\" target=\"_blank\">LARP</a>-Kampagnen und -Welten. Die Karten repräsentieren Dinge und Momente, die so im Spiel erlebt wurden."
                     },
                     {
                         "question": "Für wen ist Anor gedacht?",
-                        "answer": "Anor richtet sich an LARP-Spieler, die ihre Charaktere und Geschichten in einem Sammelkartenspiel verewigen möchten, sowie an TCG-Fans, die ein einzigartiges Kartenspiel mit Community-basierten Inhalten suchen."
+                        "answer": "Anor ist etwas für alle, die gerne Sammelkartenspiele spielen oder sammeln. Inhaltlich richtet es sich natürlich an LARP-Spieler, die ihre Charaktere und Geschichten in einem Sammelkartenspiel verewigen möchten, aber uns ist es wichtig, dass Anor auch außerhalb des LARPs und von Nicht-LARPern genauso gespielt und genossen werden kann."
                     },
                     {
-                        "question": "Kann ich eigene Charaktere ins Spiel bringen?",
-                        "answer": "Ja! Reiche Deinen LARP-Charakter bei uns ein, und wir arbeiten gemeinsam daran, ihn als individuelle Spielkarte umzusetzen."
-                    },
-                    {
-                        "question": "Wann ist Anor erhältlich?",
-                        "answer": "Im kommenden Jahr wird Anor IT sammelbar und OT erhältlich sein. Ihr könnt das Spiel aber jetzt schon online ausprobieren – kostenlos und ohne Anmeldung."
-                    },
-                    {
-                        "question": "Kann ich Anor jetzt schon spielen?",
-                        "answer": "Ja! Ihr könnt Anor sofort online spielen – kostenlos und ganz ohne Anmeldung."
+                        "question": "Wie kann ich Anor spielen?",
+                        "answer": "Zusätzlich zum physischen Spiel kann Anor auch vollkommen kostenlos und ohne Anmeldung <a href=\"https://anor.cards/tcg-arena/\" target=\"_blank\">online gespielt werden</a>. Spielpartner finden sich zahlreich auf unserem <a href=\"https://discord.gg/BVQrSYF5jg\" target=\"_blank\">Anor-Discord-Server</a>."
                     },
                     {
                         "question": "Kostet das Spiel etwas?",
-                        "answer": "Nein, Anor ist ein Non-Profit-Projekt und die Online-Version ist kostenlos verfügbar. Im kommenden Jahr wird eine gedruckte Version folgen. Etwaige Einnahmen dienen ausschließlich dazu, Drittkosten wie den Druck zu decken. Das Projekt lebt zu 100 % vom Community-Engagement."
+                        "answer": "Anor ist ein Freizeitprojekt. Die Online-Version ist vollkommen kostenlos verfügbar. Ebenso die Karten, die an größeren Cons IT erspielt werden können. Die Kosten der gedruckten Produkte decken die Druck- und Produktionskosten sowie ebendiese Querfinanzierung. Das Projekt lebt also vollkommen von der Gemeinschaft."
+                    },
+                    {
+                        "question": "Wann ist Anor erhältlich?",
+                        "answer": "<a href=\"https://w1a.notion.site/vorbestellung-anor-starter-decks\" target=\"_blank\">Die ersten Starterdecks sind schon verfügbar!</a> Zusätzlich bringen unzählige Spielmacher auf den großen Cons schon länger Karten ins Spiel, die IT erspielt werden können."
+                    },
+                    {
+                        "question": "Kann ich eigene Charaktere ins Spiel bringen?",
+                        "answer": "Ja! <a href=\"https://w1a.notion.site/1f895bba56ba8019991ec719a66e5b53?pvs=105\" target=\"_blank\">Reiche Deinen Charakter bei uns ein</a>, und wir arbeiten gemeinsam daran, ihn als individuelle Spielkarte umzusetzen."
                     },
                     {
                         "question": "Was bedeutet IT sammelbar und OT erhältlich?",
-                        "answer": "IT (In Time) sammelbar bedeutet, dass ihr die Karten im LARP-Spiel sammeln könnt. OT (Off Time) erhältlich bedeutet, dass die Karten auch außerhalb des LARPs als physische Produkte verfügbar sein werden."
+                        "answer": "<i><a href=\"https://de.wikipedia.org/wiki/Live_Action_Role_Playing#In-Time_und_Out-Time\">IT</a> sammelbar</i> bedeutet, dass die Karten im LARP-Spiel gesammelt und gehandelt werden können. Unzählige freiwillige Spielmacher bringen Karten ins Spiel, etwa als Belohnung für Aufgaben oder als Beute im Kampf. Die im Spiel sammelbaren Karten sind speziell markiert, aber natürlich mit den anderen Karten kompatibel. <i><a href=\"https://de.wikipedia.org/wiki/Live_Action_Role_Playing#In-Time_und_Out-Time\">OT</a> erhältlich</i> bedeutet, dass Karten, <a href=\"https://w1a.notion.site/vorbestellung-anor-starter-decks\">wie etwa die Starterdecks</a>, auch außerhalb des LARPs als physische Produkte angeboten werden."
                     },
                     {
-                        "question": "Ist Anor ein kommerzielles Projekt?",
-                        "answer": "Nein, Anor ist ein Non-Profit-Projekt. Alle Einnahmen dienen ausschließlich dazu, Drittkosten wie den Druck der Karten zu decken. Das Projekt lebt zu 100 % vom Community-Engagement."
+                        "question": "Wie entstehen Bilder und andere Inhalte?",
+                        "answer": "Wir machen, was wir können, selbst. So sind etwa Kartentexte, Elementsymbole und andere grafische Elemente, der ganze Balanceprozess, und auch diese Webseite etc. menschlich entstanden. Bei der Erstellung von Kartenbildern setzen wir gezielt KI-Werkzeuge ein und iterieren dann, auch durch händisches Übermalen, bis sie unseren Qualitätsansprüchen gerecht werden. Es ist uns schlicht nicht möglich, menschliche Künstler bei dieser großen Anzahl an Bildern adäquat zu entlöhnen."
                     },
                     {
-                        "question": "Warum verwendet ihr KI-generierte Bilder?",
-                        "answer": "Da wir ein Non-Profit-Projekt sind, nutzen wir KI-Technologie, um trotz begrenztem Budget hochwertige Charakterbilder für unser Kartenspiel zu erstellen. So können wir eine professionelle visuelle Qualität erreichen und das Spiel für alle zugänglich halten."
-                    },
-                    {
-                        "question": "Wie entsteht das Artwork für die Charaktere?",
-                        "answer": "Jeder Charakter beginnt mit einem Foto, das dann mit verschiedenen KI-Tools bearbeitet wird. Anschließend verfeinern wir die Bilder in Photoshop, um den finalen Look zu erreichen und sicherzustellen, dass alle Charaktere perfekt zu unserer Spielwelt passen."
+                        "question": "Wie entstehen die Charakterbilder?",
+                        "answer": "Jeder Charakter beginnt mit Referenzfotos, die die jeweilige Person einsendet. Diese Fotos werden dann von uns bearbeitet, unter anderem mithilfe von KI und anderen Werkzeugen. Anschließend verfeinern wir die Bilder, etwa in Photoshop, bis wir mit der Darstellung zufrieden sind und der Charakter perfekt in die Spielwelt passt."
                     },
                     {
                         "question": "Wie kann ich mich am Projekt beteiligen?",
-                        "answer": "Das Projekt lebt vom Community-Engagement. Ihr könnt eure Charaktere einreichen, das Spiel online testen, Feedback geben und die Community unterstützen."
+                        "answer": "Das Projekt lebt von der Gemeinschaft. So kannst Du <a href=\"https://w1a.notion.site/1db95bba56ba8052b176f05bcde11d31\" target=\"_blank\">Deinen Charakter einreichen</a>, das <a href=\"https://anor.cards/tcg-arena/\" target=\"_blank\">Spiel online testen</a> und uns <a href=\"https://w1a.notion.site/1db95bba56ba8052b176f05bcde11d31\" target=\"_blank\">Rückmeldungen geben</a>, <a href=\"https://w1a.notion.site/vorbestellung-anor-starter-decks\" target=\"_blank\">ein Starterdeck erwerben</a>, oder das Projekt anderweitig unterstützen. Am besten trittst Du dazu unserem <a href=\"https://discord.gg/BVQrSYF5jg\" target=\"_blank\">Discord-Server</a> bei."
                     }
                 ],
-                "footerText": `© ${new Date().getFullYear()} Verein Anor`,
+                "footerText": `© ${new Date().getFullYear()} Verein Anor ✦ Stand: 7. August 2026`,
                 "loadTemplate": Preprocessor.loadTemplate,
             });
             res.send(template);
